13bbfb1cd80c5da70eb964772670acdb001674b1
Takiidesuu <rickylieandro@yahoo.com>
2023-02-17 01:53:09 +0900

    CameraMove削除（Cinemachineがあるから）、ジャンプ処理追加（仮）

--- Assets/users/Ricky/Scripts/PlayerMove.cs
+++ Assets/users/Ricky/Scripts/PlayerMove.cs
@@ -13,24 +13,29 @@ public class PlayerMove : MonoBehaviour
     
     //コンポネント
     private Rigidbody rb;           //リギッドボディー
-    private Collider col;           //コライダー
+    private CapsuleCollider col;    //コライダー
+    
+    private bool is_grounded;       //地面についているか
     
     private GameObject camera_obj;  //カメラオブジェクト
     
-    private float turn_smooth_velocity; //
+    private float turn_smooth_velocity; //回転速度
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();     //リギッドボディー取得
-        col = GetComponent<Collider>();     //コライダー取得
+        col = GetComponent<CapsuleCollider>();     //コライダー取得
         camera_obj = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (is_grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
     
     void FixedUpdate() 
@@ -48,20 +53,34 @@ public class PlayerMove : MonoBehaviour
             Vector3 move_dir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
             rb.velocity = new Vector3(move_dir.normalized.x * speed, rb.velocity.y, move_dir.normalized.z * speed);
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
-            Jump();
-        }
-    }
-    
-    bool IsGrounded()
-    {
-        return Physics.CapsuleCast(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.center.y - 0.1f, col.bounds.center.z), 0.18f, Vector3.down);
     }
     
     void Jump()
     {
-        rb.AddForce(Vector3.up * jump_power, ForceMode.Impulse);
+        rb.AddForce(Vector3.up * jump_power * 2.0f, ForceMode.Impulse);
+    }
+    
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.layer == 6)
+        {
+            is_grounded = true;
+        }
+    }
+    
+    private void OnCollisionStay(Collision other) 
+    {
+        if (other.gameObject.layer == 6)
+        {
+            is_grounded = true;
+        }
+    }
+    
+    private void OnCollisionExit(Collision other) 
+    {
+        if (other.gameObject.layer == 6)
+        {
+            is_grounded = false;
+        }
     }
 }
