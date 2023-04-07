7596fe5d531eda56b75c6c35e0dd20a7497bb09b
Edamame46 <naoki04351@yahoo.co.jp>
2023-04-07 19:24:10 +0900

    ＣＬＥＡＲ追加

---
+++ Assets/Users/Hirata/Scripts/StageClearCheck.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageClearCheck : MonoBehaviour
+{
+    GameObject ClearCanvas;
+
+    List<LightBulb> lightBulbs = new List<LightBulb>();
+
+    bool once = true;
+
+    void Start()
+    {
+        ClearCanvas = (GameObject)Resources.Load("ClearCanvas");
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("LightBulb");
+
+        foreach(GameObject gameObject in gameObjects)
+        {
+            lightBulbs.Add(gameObject.GetComponent<LightBulb>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (once)
+        {
+            bool Is_StageClear = true;
+            foreach (LightBulb gameObject in lightBulbs)
+            {
+                if (Is_StageClear)
+                {
+                    if (!gameObject.is_stage_hit)
+                    {
+                        Is_StageClear = false;
+                    }
+                }
+            }
+            if (Is_StageClear)
+            {
+                Instantiate(ClearCanvas);
+                once = false;
+            }
+        }
+    }
+}
