82bb587a5f7e08bb1a0968c8249883e3a9e93e2f
UmataHaruto <umataharuto@gmail.com>
2023-04-09 19:53:22 +0900

    回転壁プレハブの生成

--- Assets/Users/Umata/Scripts/Rotator.cs
+++ Assets/Users/Umata/Scripts/Rotator.cs
@@ -4,16 +4,17 @@ public class Rotator : MonoBehaviour
 {
     public float rotate_speed = 10f;
     public GameObject[] rotate_objects;  //回転対象オブジェクト
-    public GameObject trigger_switch;    //スイッチオブジェクト
 
     private GameObject modeldata;    //モデルデータ
     public bool is_rotate = true;   //1 = 回転
     public Material active_material; //起動時マテリアル
     public Material inactive_material;  //非稼働時マテリアル
     private Renderer object_renderer;
+    Rotator_switch trigger_switch;    //スイッチオブジェクト
 
     void Start()
     {
+        trigger_switch = transform.Find("Switch").gameObject.GetComponent<Rotator_switch>();
         modeldata = transform.Find("model").gameObject;
         object_renderer = modeldata.GetComponent<Renderer>();
         UpdateMaterial();
@@ -28,6 +29,7 @@ public class Rotator : MonoBehaviour
                 obj.transform.RotateAround(transform.position, Vector3.forward, rotate_speed * Time.deltaTime);
             }
         }
+        is_rotate = trigger_switch.is_stage_hit;
     }
 
     void OnValidate()
--- Assets/Users/Umata/Scripts/Rotator_switch.cs
+++ Assets/Users/Umata/Scripts/Rotator_switch.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class Rotator_switch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool is_stage_hit = false;   //ステージに当たっているか
+    private int nothit_count;           //当たってらずに何フレーム立ったか (Exitが呼ばれないため)
+
+    private DeformStage deform_stage;    //電源がステージが当たっているかを取得
+    public Material on_power_material;    //マテリアルを変更
+    public Material off_power_material;    //マテリアルを変更
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //ステージに当たっていなければフラグを変える
+        if (is_stage_hit)
+        {
+            if (nothit_count > 5)
+            {
+                is_stage_hit = false;
+                deform_stage = null;
+            }
+            else
+                nothit_count++;
+        }
+
+        //どちらもステージに当たっているか
+        if (deform_stage != null)
+        {
+            if (deform_stage.hit_electrical && is_stage_hit)
+            {
+            }
+            else
+            {
+                is_stage_hit = false;
+            }
+        }
+        if(is_stage_hit)
+        {
+            gameObject.GetComponent<Renderer>().material = on_power_material;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material = off_power_material;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("ElectricalBall"))
+        {
+            is_stage_hit = true;
+            nothit_count = 0;
+        }
+        if (other.gameObject.layer == 6 && is_stage_hit)
+        {
+            deform_stage = other.transform.root.GetComponent<DeformStage>();
+            nothit_count = 0;
+        }
     }
 }
