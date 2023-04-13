92ac196e7b4ebcda89450e09e138e1cfd0e101e4
Takiidesuu <rickylieandro@yahoo.com>
2023-04-13 15:37:35 +0900

    ダメージスクリプトCollisionEnterにした

--- Assets/Users/Ricky/Scripts/DamageScript.cs
+++ Assets/Users/Ricky/Scripts/DamageScript.cs
@@ -28,7 +28,7 @@ public class DamageScript : MonoBehaviour
         
     }
     
-    private void OnTriggerEnter(Collider other) 
+    private void OnCollisionEnter(Collision other) 
     {
         if (other.gameObject.tag == "ElectricalBall")
         {
