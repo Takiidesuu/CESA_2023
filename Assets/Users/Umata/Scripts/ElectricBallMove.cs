0aa6464358f934024c3d4242890a84c41ada9d24
UmataHaruto <umataharuto@gmail.com>
2023-04-28 17:33:07 +0900

    エレキボール
    坂制御

--- Assets/Users/Umata/Scripts/ElectricBallMove.cs
+++ Assets/Users/Umata/Scripts/ElectricBallMove.cs
@@ -13,6 +13,12 @@ public class ElectricBallMove : MonoBehaviour
     [Tooltip("消滅時間")]
     [SerializeField] private float m_destroy_time = 5.0f;
 
+    [Tooltip("坂での加速時間")]
+    [SerializeField] private float m_accelerator_time = 1.0f;
+
+    [Tooltip("坂での加速量")]
+    [SerializeField] private float m_accelerator_speed = 2.0f;
+
     [Tooltip("回転の滑らかさ")]
     [SerializeField] private float turn_smooth_time = 1.0f;
 
@@ -48,7 +54,7 @@ public class ElectricBallMove : MonoBehaviour
         Vector3 playerpos;
         playerpos.x = transform.position.x;
         playerpos.y = transform.position.y;
-        playerpos.z = player.transform.position.z;
+        playerpos.z = 0;
         //Z軸を強制的にPlayer座標に設定
         transform.position = playerpos;
     }
@@ -61,6 +67,37 @@ public class ElectricBallMove : MonoBehaviour
                 Destroy(this.gameObject);
             }
         }
+        if (collision.gameObject.tag == "SpeedBooster")
+        {
+            BoostSpeed(m_accelerator_time,m_accelerator_speed);
+        }
+
+    }
+    public void BoostSpeed(float boostTime, float boostSpeed)
+    {
+        StartCoroutine(BoostSpeedCoroutine(boostTime, boostSpeed));
     }
 
+    private IEnumerator BoostSpeedCoroutine(float boostTime, float boostSpeed)
+    {
+        float originalSpeed = m_speed; // 元の速度を保存する
+
+        m_speed += boostSpeed; // 速度を増加させる
+
+        yield return new WaitForSeconds(boostTime); // 指定時間待つ
+
+        // 元の速度に戻るまでの時間
+        float decelerationTime = 0.5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < decelerationTime)
+        {
+            float t = elapsedTime / decelerationTime;
+            m_speed = Mathf.Lerp(m_speed, originalSpeed, t); // 現在の速度から元の速度に徐々に戻す
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        m_speed = originalSpeed; // 元の速度に戻す
+    }
 }
--- Assets/users/Ricky/Scripts/PlayerMove.cs
+++ Assets/users/Ricky/Scripts/PlayerMove.cs
@@ -45,9 +45,11 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float smash_max_time = 5.0f;
     [Header("叩く力")]
     [SerializeField] private float smash_power_scalar = 3.0f;
-    
+    [Header("加速用プレハブ")]
+    [SerializeField] public GameObject SpeedBooster;
     [SerializeField] private GameObject blackPanel;
-    
+    [SerializeField] private Vector3 foot_pos;
+
     //コンポネント
     private Rigidbody rb;                   //リギッドボディー
     private CapsuleCollider col;            //コライダー
@@ -577,6 +579,8 @@ public class PlayerMove : MonoBehaviour
             ground_obj_parent = ground_obj.transform.root.gameObject;
             deform_stage = ground_obj_parent.GetComponent<DeformStage>();
             min_max_deform = ground_obj_parent.GetComponent<MinMaxDeform>();
+            //足元の位置を保存
+            foot_pos = hit.point;
         }
         else
         {
@@ -623,6 +627,8 @@ public class PlayerMove : MonoBehaviour
 
         //叩くSEの再生
         soundmanager.PlaySoundEffect("Strike");
+        //Instantiate(SpeedBooster,foot_pos, Quaternion.identity);
+
         if (deform_stage)
         {
 
