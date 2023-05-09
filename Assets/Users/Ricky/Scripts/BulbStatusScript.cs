2e8c1856286d69e11f4b3e2b57136a2bfb5e305d
Takiidesuu <rickylieandro@yahoo.com>
2023-05-09 19:19:33 +0900

    電球のUI作成

--- Assets/Users/Ricky/Scripts/BulbStatusScript.cs
+++ Assets/Users/Ricky/Scripts/BulbStatusScript.cs
@@ -5,17 +5,50 @@ using UnityEngine.UI;
 
 public class BulbStatusScript : MonoBehaviour
 {
-    private Image[] progress_bar;
+    [Tooltip("BulbLineプレハブ")]
+    [SerializeField] private GameObject bulb_line_obj_template;
+    
+    private Vector2 background_size = new Vector2(500.0f, 250.0f);
+    
+    private List<BulbLineScript> progress_bar;
+    private LightBulbCollector collector;
+    
+    private int num_of_bulbs;
+    
+    public void AddStatus(LightBulb bulb)
+    {
+        GameObject new_line_obj = Instantiate(bulb_line_obj_template, Vector3.zero, Quaternion.identity);
+        new_line_obj.transform.parent = this.transform;
+        
+        new_line_obj.GetComponent<BulbLineScript>().current_bulb = bulb;
+        
+        progress_bar.Add(new_line_obj.GetComponent<BulbLineScript>());
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        collector = GameObject.FindObjectOfType<LightBulbCollector>();
         
+        num_of_bulbs = collector.LightBulb_num;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float space = background_size.y / (float)num_of_bulbs;
+        float y_pos = background_size.y / 2.0f * -1.0f;
+        if (num_of_bulbs % 2 == 0)
+        {
+            y_pos += space / 2.0f;
+        }
         
+        for (int i = progress_bar.Count; i >= 0; i--)
+        {
+            RectTransform rect_transform = progress_bar[i].gameObject.GetComponent<RectTransform>();
+            rect_transform.localPosition = new Vector3(rect_transform.localPosition.x, y_pos, rect_transform.localPosition.z);
+            
+            y_pos += space;
+        }
     }
 }
