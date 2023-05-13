66bb5fa55adc9209e515ea92983d119af9e1874f
UmataHaruto <umataharuto@gmail.com>
2023-05-13 13:06:17 +0900

    シェーダーを追加

--- Assets/Users/Umata/Scripts/SoundManager.cs
+++ Assets/Users/Umata/Scripts/SoundManager.cs
@@ -33,6 +33,21 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    public void PlayHitStopSound(string name,float stoptime)
+    {
+        SoundEffect effect = GetSoundEffectByName(name);
+        if (effect.clip != null)
+        {
+            int randomIndex = Random.Range(0, effect.clip.Length);
+            audioSource.clip = effect.clip[randomIndex];
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Sound effect not found.");
+        }
+    }
+
     private SoundEffect GetSoundEffectByName(string name)
     {
         for (int i = 0; i < soundEffects.Length; i++)
