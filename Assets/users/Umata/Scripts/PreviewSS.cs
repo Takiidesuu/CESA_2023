e368d83fedbddb17c32bcd029aed9018b4ade503
Edamame46 <naoki04351@yahoo.co.jp>
2023-05-27 17:37:33 +0900

    セレクト画面画像更新

--- Assets/users/Umata/Scripts/PreviewSS.cs
+++ Assets/users/Umata/Scripts/PreviewSS.cs
@@ -10,7 +10,7 @@ public class PreviewSS : MonoBehaviour
 
     private void Start()
     {
-        // ステージ1fewfefwffwif
+        // ステージ1f
         //EditorSceneManager.OpenScene("Assets/users/Pak/world1/Stage1-1.unity", OpenSceneMode.Single);
 
         // カメラが設定されていない場合、デフォルトのメインカメラを使用する
